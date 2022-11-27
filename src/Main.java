d5708a91214743555820675be6f9fdc7fa35bec8
Jappananny <Jappananny@gmail.com>
2022-11-27 15:23:44 +0300

    ДЗ 2.9/2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ProductException {
         Phonebook phonebook = new Phonebook();
         phonebook.addPhone(new FullName("Василий","Свечкин"),"79784548090");
         phonebook.addPhone(new FullName("Зая",""),"112");
@@ -28,5 +28,27 @@ public class Main {
         for (Map.Entry<FullName, String> phone : phonebook.getPrhone()) {
             System.out.println(phone.getKey() + " " + phone.getValue());
         }
+        System.out.println("==============================================================================");
+
+        Product bananas = new Product("Банан",89.90,0.5);
+        System.out.println(bananas);
+        Product nail = new Product("Деревянные гвозди", 820,3.0);
+        System.out.println(nail);
+        Product rice = new Product("Рис",119.99,10.5);
+        System.out.println(rice);
+        Product sand = new Product("Песок строительный",6, 4500.0);
+        System.out.println(sand);
+        Product mayonese = new Product("Майонез",96.50,1.0 );
+        System.out.println(mayonese);
+        ProductServise allProducts = new ProductServise();
+        allProducts.add(bananas);
+        allProducts.add(nail);
+        allProducts.add(mayonese);
+        Recipes fairNails = new Recipes("Жаренные гвозди");
+        fairNails.addProd(nail,5);
+        fairNails.addProd(mayonese,1);
+        System.out.println(fairNails.sumPricesRecipes() + " Итого сумма");
+
+
     }
 }
